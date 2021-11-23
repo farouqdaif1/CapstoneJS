e495818ac889437d8746e711cb3572f85dbfbf9d
farouqdaif1 <daiffarouq@gmail.com>
2021-11-23 16:51:53 +0200

    Add render function

---
+++ src/counter.js
@@ -0,0 +1,7 @@
+// import getMeals from './consume-api';
+
+const counter = (mealsList) => {
+    console.log(mealsList.children);
+};
+// console.log(counter());
+export default counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import './style.css';
+import getMeals from './consume-api';
+import counter from './counter';
+// render 6 meals from api
+const mealsList = document.querySelector('.meals');
 
-function component() {
-  const element = document.createElement('div');
-  // Lodash, now imported by this script
-
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const render = async () => {
+  const dataFromAPI = await getMeals();
+  const { meals } = dataFromAPI;
+  meals.forEach((element, index) => {
+    if (index < 6) {
+      mealsList.innerHTML += `
+      <li>
+      <figure>
+      <img src="${element.strMealThumb}" alt="meal photo">
+      <figcaption>${element.strMeal}</figcaption>
+      </figure>
+      </li>`;
+    }
+  });
+};
+document.addEventListener('DOMContentLoaded', async () => {
+  await render();
+});
+counter(mealsList);
+// console.log();
